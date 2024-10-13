d924cd5801cff30de3dba2de8c19dfb7708dc817
ArtemStolbchenko <stolbchenko.artem@gmail.com>
2024-10-13 16:05:46 +0200

    The skeleton of the application
    Implemented basic routing using Slim and database initialization

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 
-require 'vendor/autoload.php';
+require './vendor/autoload.php';
+require './src/configuration/dbconfig.php';
 
 $app = new \Slim\App;
 $app->get('/', function () {
-	echo 'Welcome to the insane slim app';
+	echo 'Welcome to the insane slim app <br/>';
+	echo '<a href = "groups">Press here to view existing groups</a>';
+});
+$app->get('/groups', function()
+{
+	require './src/pages/groups.php';
+});
+$app->get('/chat', function ($groupId)
+{
+	require './src/pages/chat.php';
 });
 $app->run();
 ?>
